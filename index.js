84c191858eaccf0393b173dd3c1fc7c0687228df
gustavo <gurbrillinger@gmail.com>
2016-04-02 17:57:37 -0300

    arrumado erros, mas ainda precisa concertar erro de .push()

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var rtcLogin = require('./rtc/rtcLoginManager.js');
-var banco = require('./db/');
+//var banco = require('./db/');
 var fs = require('fs');
 var busboy = require('connect-busboy');
 
--- public/scripts/controllers/homeController.js
+++ public/scripts/controllers/homeController.js
@@ -1,5 +1,9 @@
 /**
- * Created by Osvaldo on 23/09/15.
+ * Created by Osvaldo & Gustavo on 23/09/15.
+ * 
+ * O melhor resultado é definide pelo somatorio do custo(custo dos movementos para  chegar na posição final)
+ * mais o custo estimado que é o somatorio da distancia da peça até sua posição final mais a distancia da
+ * peça vazia até a peça atual se ela não estiver em sua posição final.
  */
 
 app.controller("homeController",['$scope', function ($scope) {
@@ -34,15 +38,21 @@ app.controller("homeController",['$scope', function ($scope) {
 		22: ""
 	};
 
+	//jogadas que ja foram verificadas
 	$scope.jogadasVerificadas = [];
+	//jogadas da fronteira
 	$scope.possiveisJogadas = [];
+	//o melhor caminho para o resultado
 	$scope.menorCaminho = [];
 
 	//percorre a fronteira e verifica o elemento de menor custo
 	me.calculaCaminho = function(){
-		console.log("aqui custo", $scope.possiveisJogadas);
+
 		var menorCusto = $scope.possiveisJogadas[0].custo + $scope.possiveisJogadas[0].custoEstimado;
 		var posicao = 0;
+
+		console.log("fronteira", $scope.possiveisJogadas);
+
 		for(var index in $scope.possiveisJogadas){
 			var custoTotal = $scope.possiveisJogadas[index].custo + $scope.possiveisJogadas[index].custoEstimado;
 			if(menorCusto > custoTotal){
@@ -55,10 +65,13 @@ app.controller("homeController",['$scope', function ($scope) {
 		$scope.jogadasVerificadas.push($scope.possiveisJogadas[posicao]);
 		$scope.possiveisJogadas.splice(posicao, 1);
 
-		if(ultimaJogada == me.estadoFinal){	
+
+		if(ultimaJogada.custoEstimado == 0){
+			console.log('!fim');
 			me.calcMenorCaminho(ultimaJogada);
 		} else {
-			me.proximasJogadas(ultimaJogada);
+			console.log('proxima fronteira', ultimaJogada);
+			// me.proximasJogadas(ultimaJogada);
 		}
 		
 	};
@@ -71,11 +84,14 @@ app.controller("homeController",['$scope', function ($scope) {
 		var novaJogada = {}
 		var novoEstado = {}
 
+
 		for(var index in jogada.estado){
+			console.log('passei por isso', index)
 			if(jogada.estado[index] == ""){
 				coluna = index%10;
 				linha = (index - coluna)/10;
 
+				console.log('o vazio esta aqui', index);
 
 				if(coluna < 2){
 					//o espaço vazio pode passar para a direita
@@ -85,10 +101,13 @@ app.controller("homeController",['$scope', function ($scope) {
 
 					novaJogada = {
 						estado: novoEstado,
-						custoEstimado: me.calcCustoEstimado(novoEstado),
 						custo: jogada.custo+1,
 						pai: $scope.jogadasVerificadas.length-1
 					};
+
+					me.calcCustoEstimado(novoEstado, function(ret){
+						novaJogada.custoEstimado = ret;
+					});
 
 					$scope.possiveisJogadas.push(novaJogada);
 				}
@@ -100,10 +119,13 @@ app.controller("homeController",['$scope', function ($scope) {
 
 					novaJogada = {
 						estado: novoEstado,
-						custoEstimado: me.calcCustoEstimado(novoEstado),
 						custo: jogada.custo+1,
 						pai: $scope.jogadasVerificadas.length-1
 					};
+
+					me.calcCustoEstimado(novoEstado, function(ret){
+						novaJogada.custoEstimado = ret;
+					});
 
 					$scope.possiveisJogadas.push(novaJogada);
 				}
@@ -115,10 +137,13 @@ app.controller("homeController",['$scope', function ($scope) {
 
 					novaJogada = {
 						estado: novoEstado,
-						custoEstimado: me.calcCustoEstimado(novoEstado),
 						custo: jogada.custo+1,
 						pai: $scope.jogadasVerificadas.length-1
 					};
+
+					me.calcCustoEstimado(novoEstado, function(ret){
+						novaJogada.custoEstimado = ret;
+					});
 
 					$scope.possiveisJogadas.push(novaJogada);
 				}
@@ -130,10 +155,13 @@ app.controller("homeController",['$scope', function ($scope) {
 
 					novaJogada = {
 						estado: novoEstado,
-						custoEstimado: me.calcCustoEstimado(novoEstado),
 						custo: jogada.custo+1,
 						pai: $scope.jogadasVerificadas.length-1
 					};
+
+					me.calcCustoEstimado(novoEstado, function(ret){
+						novaJogada.custoEstimado = ret;
+					});
 
 					$scope.possiveisJogadas.push(novaJogada);
 				}
@@ -141,7 +169,9 @@ app.controller("homeController",['$scope', function ($scope) {
 			}
 		}
 
-		me.calculaCaminho();
+		console.log('os novos possiveis', $scope.possiveisJogadas);
+
+		//me.calculaCaminho();
 	};
 
 	//percorrer jogadasVerificadas para encontrar o menor caminho e salvar rota em menorCaminho
@@ -150,18 +180,19 @@ app.controller("homeController",['$scope', function ($scope) {
 
 		var jogadaAnterior = jogadaFinal;
 		for(var x = jogadaFinal.custo; x>=0; x++){
-			
 			$scope.menorCaminho.push(jogadaAnterior.estado);
+			if(jogadaAnterior.pai == ""){
+				break;
+			}
 			jogadaAnterior = $scope.jogadasVerificadas[jogadaAnterior.pai];
-
-
 		}
 
 		console.log($scope.menorCaminho);
 	};
 
 	//calcula custo estimado
-	me.calcCustoEstimado = function(estado){
+	me.calcCustoEstimado = function(estado, callback){
+
 		var colunaDesejada = 0;
 		var linhaDesejada = 0;
 
@@ -173,45 +204,42 @@ app.controller("homeController",['$scope', function ($scope) {
 
 		var custoTotal = 0;
 
+		for(var index in estado){
+			if(estado[index] == ""){
+				colunaDoVazio = index%10;
+				linhaDoVazio = (index - colunaDoVazio)/10;		
+			}
+		}
+
 		for(var numero in me.estadoFinal){
 			colunaDesejada = numero%10;
 			linhaDesejada = (numero - colunaDesejada)/10;
 
 			for(var index in estado){
 
-				if(me.estadoFinal[numero] == estado[index] && numero == index){
+				if(me.estadoFinal[numero] == estado[index] && numero != index){
 					//calc do  num atual ate sua posicao final
-					if(estado[index] == me.estadoFinal[numero]){
+					colunaAtual = index%10;
+					linhaAtual = (index - colunaAtual)/10;
 
-						colunaAtual = index%10;
-						linhaAtual = (index - colunaAtual)/10;
+					custoTotal += Math.abs(colunaDesejada - colunaAtual);
+					custoTotal += Math.abs(linhaDesejada - linhaAtual);
 
-						custoTotal += Math.abs(colunaDesejada - colunaAtual);
-						custoTotal += Math.abs(linhaDesejada - linhaAtual);
-
-					}
-
-					//calc do elemento vazio ate num atual 
-					if(estado[index] == ""){
-						
-						colunaDoVazio = index%10;
-						linhaDoVazio = (index - colunaDoVazio)/10;
-
-						custoTotal += Math.abs(colunaAtual - colunaDoVazio);
-						custoTotal += Math.abs(linhaAtual - linhaDoVazio);
-
-					}
+					//calc do elemento vazio ate num atual
+					custoTotal += Math.abs(colunaAtual - colunaDoVazio);
+					custoTotal += Math.abs(linhaAtual - linhaDoVazio);				
+					
 				}
-
 			}
 		}
 
-		return custoTotal;
+		console.log('calculando', custoTotal);
+		callback(custoTotal);
 	};
 
 
 	//-----------------------------------------VALIDADOR--------------------------------------------------
-	me.numerosEscolhidos = []
+	me.numerosEscolhidos = [];
 
 	//verifica se o numero necessario foi usado e salva em numerosEscolhidos
 	me.verificaNumerosEscolhidos = function(numero){
@@ -225,23 +253,40 @@ app.controller("homeController",['$scope', function ($scope) {
 
 	//verifica se foi dada uma sequencia de numeros valida e começa a calcular caminho
 	$scope.verificaValido = function(){
+		$scope.jogadasVerificadas = [];
+		$scope.possiveisJogadas = [];
+		$scope.menorCaminho = [];
 		me.numerosEscolhidos = [];
 		for(var index in me.estadoFinal){
 			me.verificaNumerosEscolhidos(me.estadoFinal[index]);
 		}
 
 		if(me.numerosEscolhidos.length == 9){
-			$scope.possiveisJogadas = [
-				{
+
+			me.primeiro = {
 					estado: $scope.posicaoAtual,
-					custoEstimado: me.calcCustoEstimado($scope.posicaoAtual),
+					custoEstimado: 0,
 					custo: 0,
 					pai: ""
-				}
-			];
+				};
 
-			me.calculaCaminho();
+			me.calcCustoEstimado($scope.posicaoAtual, function(ret){
+				console.log(ret);
+				me.primeiro.custoEstimado = ret;
+
+				$scope.possiveisJogadas.push(me.primeiro);
+
+				console.log('o primeiro', me.primeiro);
+				
+				console.log("numeros escolhidos", me.numerosEscolhidos);
+
+				me.calculaCaminho();
+			});
+
+
 		}
+
+
 	};
 	//-------------------------------------------------------------------------------------------------------
 
